cf557765eb08412aca9e67f568e90e4d6d007ee0
Ubuntu <ubuntu@ip-172-31-21-116.ca-central-1.compute.internal>
2024-12-16 16:42:38 +0000

    initial commit

--- application-code/web-tier/src/components/DatabaseDemo/DatabaseDemo.js
+++ application-code/web-tier/src/components/DatabaseDemo/DatabaseDemo.js
@@ -21,7 +21,7 @@
           }
 
         populateData(){
-            this.fetch_retry('http://35.183.14.187:4000/transaction',3)
+            this.fetch_retry('http://15.222.1.180:4000/transaction',3)
             .then(res => res.json())
             .then((data) => {
               this.setState({ transactions : data.result });
@@ -59,7 +59,7 @@
            const requestOptions = {
                method: 'DELETE'
            }
-           fetch('http://35.183.14.187:4000/transaction', requestOptions)
+           fetch('http://15.222.1.180:4000/transaction', requestOptions)
            .then(response => response.json())
            .then(data => this.populateData())
 
@@ -76,7 +76,7 @@
                 body: JSON.stringify({"amount":this.state.text_amt, "desc" :this.state.text_desc})
             }
             
-            fetch('http://35.183.14.187:4000/transaction', requestOptions)
+            fetch('http://15.222.1.180:4000/transaction', requestOptions)
             .then(response => response.json())
             .then(data => this.populateData())
             
